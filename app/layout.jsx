69679079574bf3ecbd529898359f2952d79bca68
AhmedElshenawy0 <shenawy100@gmail.com>
2026-05-10 18:55:21 +0300

    change quick questions

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,11 @@
 import { Cairo } from "next/font/google";
 import "./globals.css";
 import QueryProvider from "./components/QueryProvider";
+import { Suspense } from "react";
 
 const cairo = Cairo({
   subsets: ["latin", "arabic"],
-  weight: ["300", "400", "600", "700", "800", "900", "1000"],
+  weight: ["300", "400", "600", "700", "800", "900"],
   variable: "--font-cairo",
   display: "swap",
 });
@@ -21,7 +22,9 @@ export default function RootLayout({ children }) {
         className={`${cairo.variable} font-sans antialiased bg-[#f8f9fd] text-[#191c1f]`}
       >
         <QueryProvider>
-          <main>{children}</main>
+          <Suspense fallback={<div>Loading...</div>}>
+            <main>{children}</main>
+          </Suspense>
         </QueryProvider>
       </body>
     </html>
